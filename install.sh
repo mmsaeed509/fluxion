3b88a8dc18232b174116d1e2b837ae48abb3ab03
Mahmoud Mohammed <62524855+mmsaeed509@users.noreply.github.com>
2022-01-14 09:06:31 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+yes | sudo pacman -S aircrack-ng dhcp lighttpd macchanger nmap xterm php-cgi debtap
+
+
+echo "installing mdk3..... "
+
+
+git clone https://github.com/Info-security/mdk3.git
+
+
+cd /mdk3-master
+
+make
+
+sudo make install
+
+cd ..
+
+echo "installing pyrit..... "
+
+wget -c https://github.com/JPaulMora/Pyrit/archive/v0.5.0.tar.gz
+tar -xf v0.5.0.tar.gz
+cd Pyrit-0.5.0
+sed -i "s/COMPILE_AESNI/NO_COMPILE_AESNI/" cpyrit/_cpyrit_cpu.c
+python2 setup.py build
+sudo python2 setup.py install
